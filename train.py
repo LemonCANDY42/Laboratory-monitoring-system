113f068a5c5157455c16b76369c9b55866281f18
Kenny <l.w.r.f.42@gmail.com>
2021-12-04 16:19:55 +0800

    update model add requirements

--- train.py
+++ train.py
@@ -81,14 +81,15 @@ datamodule = VideoClassificationData.from_fiftyone(
 # 0. List the available models
 print(VideoClassifier.available_backbones())
 # out: ['efficient_x3d_s', 'efficient_x3d_xs', ... ,slowfast_r50', 'x3d_m', 'x3d_s', 'x3d_xs']
-print(VideoClassifier.get_backbone_details("x3d_xs"))
+print(VideoClassifier.get_backbone_details("x3d_m"))
 
 # # 2. Build the task
-# model = VideoClassifier(backbone="x3d_xs", num_classes=datamodule.num_classes, pretrained=True)
+model = VideoClassifier(backbone="x3d_m", num_classes=datamodule.num_classes, pretrained=True)
 
-model = VideoClassifier.load_from_checkpoint("video_classification.pt")
+# model = VideoClassifier.load_from_checkpoint("video_classification.pt")
 # 3. Create the trainer and finetune the model
 trainer = flash.Trainer(max_epochs=5, gpus=torch.cuda.device_count())
+model.serializer = FiftyOneLabels(return_filepath=True)
 trainer.finetune(model, datamodule=datamodule, strategy="no_freeze")#no_freeze
 # trainer.fit(model, datamodule=datamodule)
 filepaths = all_path("/home/kenny/github/Laboratory-monitoring-system/videos/1")
@@ -96,7 +97,6 @@ predictions = model.predict(filepaths)
 print(predictions)
 # 5. Save the model!
 trainer.save_checkpoint("video_classification.pt")
-
 
 # classifier = VideoClassifier.load_from_checkpoint("video_classification.pt")
 # classifier.serializer = FiftyOneLabels(return_filepath=True)
