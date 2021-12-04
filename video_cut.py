50f61a6b20b3dce68a0ae7b41f0361a64688e4de
Kenny <l.w.r.f.42@gmail.com>
2021-12-04 11:44:07 +0800

    update video clip

--- video_cut.py
+++ video_cut.py
@@ -4,40 +4,22 @@ import os
 
 from func import all_path
 
-path = "/home/kenny/github/Laboratory-monitoring-system/videos/train/stop"
+
+path = "/home/kenny/github/Laboratory-monitoring-system/videos/train/other"
 # 获得视频对象 
 in_path = path
 names = os.listdir(in_path)
-out_path = path
+split_time = 2
 for name in names:
     index = name.rfind('.')
     name = name[:index]
-    videoCapture = cv2.VideoCapture(in_path+'/'+name+'.mp4')
-    if not os.path.exists(out_path+'/'+name): # 判断文件夹是否已经存在    
-        os.mkdir(out_path+'/'+name)
-    # 获得码率及尺寸
-    fps = videoCapture.get(cv2.CAP_PROP_FPS)
-    size = (int(videoCapture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(videoCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-    fNUMS = videoCapture.get(cv2.CAP_PROP_FRAME_COUNT)
-    print('fps {}  size {} nums {}'.format(fps, size, fNUMS))
-
-    # 当成功打开视频时cap.isOpened()返回True,否则返回False
-    if videoCapture.isOpened(): 
-        time = 2 # 每隔10秒截取一段
-        frame_index = 0 # 当前帧
-        while (True):
-            success, frame = videoCapture.read()
-            if success:
-                frame_index += 1
-                if (frame_index % (fps*time) == 1):
-                    videoWriter = cv2.VideoWriter(out_path+'/'+name+'/'+str(int(frame_index//(fps*time))) +'.mp4',
-                                                cv2.VideoWriter_fourcc(*"mp4v"), fps,size)
-                    videoWriter.write(frame)
-                    print(out_path+'/'+name+'/'+str(int(frame_index//(fps*time))) +'.mp4')
-                else:
-                    videoWriter.write(frame)
-            else:
-                print('end')
-                break
-
-    videoCapture.release()
+    end = '.mp4'
+    os.chdir(path)
+    os.getcwd()
+    # videoCapture = cv2.VideoCapture(in_path+'/'+name+'.mp4')
+    out_path = './'+name+'/'
+    if not os.path.exists(out_path): # 判断文件夹是否已经存在    
+        os.mkdir(out_path)
+    print(r'ffmpeg -i {0}{2} -threads 8 -vcodec copy -f segment -segment_time {3} {4}{1}%04d{2}'.format(name,name,end,split_time,out_path))  
+    output = os.popen(r'ffmpeg -i {0}{2} -threads 8 -vcodec copy -f segment -segment_time {3} {4}{1}%04d{2}'.format(name,name,end,split_time,out_path)).readlines()
+    
