5662a8cad0d2d229df39e0a97e5f9fedb5ffd132
Kenny <l.w.r.f.42@gmail.com>
2021-12-04 14:04:14 +0800

    fixed bug

--- video_cut.py
+++ video_cut.py
@@ -1,15 +1,13 @@
-import numpy as np
-import cv2
 import os
 
 from func import all_path
 
 
-path = "/home/kenny/github/Laboratory-monitoring-system/videos/train/other"
+path = "/home/kenny/github/Laboratory-monitoring-system/videos/train/stop"
 # 获得视频对象 
 in_path = path
 names = os.listdir(in_path)
-split_time = 2
+split_time = '00:00:02.000' 
 for name in names:
     index = name.rfind('.')
     name = name[:index]
@@ -20,6 +18,6 @@ for name in names:
     out_path = './'+name+'/'
     if not os.path.exists(out_path): # 判断文件夹是否已经存在    
         os.mkdir(out_path)
-    print(r'ffmpeg -i {0}{2} -threads 8 -vcodec copy -f segment -segment_time {3} {4}{1}%04d{2}'.format(name,name,end,split_time,out_path))  
-    output = os.popen(r'ffmpeg -i {0}{2} -threads 8 -vcodec copy -f segment -segment_time {3} {4}{1}%04d{2}'.format(name,name,end,split_time,out_path)).readlines()
+    print(r'ffmpeg -i {0}{2} -threads 8 -map 0  -c:v libx264  -f segment -segment_time {3} {4}{1}%04d{2}'.format(name,name,end,split_time,out_path))  
+    output = os.popen(r'ffmpeg -i {0}{2} -threads 8  -c copy -flags +global_header -f segment  -segment_time {3} -segment_format_options movflags=+faststart -reset_timestamps 1 {4}{1}%d{2}'.format(name,name,end,split_time,out_path)).readlines()
     
