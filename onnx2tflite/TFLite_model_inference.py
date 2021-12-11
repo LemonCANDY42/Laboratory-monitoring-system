aa630e1025583606c12387de5bee85c87b24d169
windows10 <l.w.r.f.42@gmail.com>
2021-12-12 01:20:44 +0800

    add pi tflite

--- onnx2tflite/TFLite_model_inference.py
+++ onnx2tflite/TFLite_model_inference.py
@@ -11,6 +11,7 @@ output_details = interpreter.get_output_details()
 
 # Test the model on random input data
 input_shape = input_details[0]['shape']
+print(input_shape)
 input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
 interpreter.set_tensor(input_details[0]['index'], input_data)
 
