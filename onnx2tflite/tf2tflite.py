3710b0291f973d7191b57f447048d1f3c06a2f2b
windows10 <l.w.r.f.42@gmail.com>
2021-12-11 20:07:53 +0800

    add tflite supported_ops

--- onnx2tflite/tf2tflite.py
+++ onnx2tflite/tf2tflite.py
@@ -6,8 +6,11 @@ converter = tf.lite.TFLiteConverter.from_saved_model('models/x3d_m')
 
 #使用动态范围量化进行转换
 #现在，我们启用默认的 optimizations 标记来量化所有固定参数（例如权重
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
-converter.target_spec.supported_types = [tf.float16]
+# converter.optimizations = [tf.lite.Optimize.DEFAULT]
+# converter.target_spec.supported_types = [tf.float16,]
+converter.target_spec.supported_ops = [tf.float16,
+                                       tf.lite.OpsSet.TFLITE_BUILTINS,
+                                       tf.lite.OpsSet.SELECT_TF_OPS]
 tflite_model = converter.convert()
 
 # Save the model
