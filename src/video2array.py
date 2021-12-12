d1f034a379fad8241c6492f907383f9457c5b3b9
windows10 <l.w.r.f.42@gmail.com>
2021-12-13 05:02:10 +0800

    量化到8bit

--- src/video2array.py
+++ src/video2array.py
@@ -1,26 +1,37 @@
 import cv2
 import numpy as np
+import sys
+sys.path.append("..") 
+from func import all_path
 
-for
+def video_to_array(path = '../video/New directory'):
+    video_array = []
+    for i in all_path(path):
 
-cap = cv2.VideoCapture('./video/New directory/0.mp4')
-frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        cap = cv2.VideoCapture(i)
+        frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        count = frameCount//16
+        frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-buf = np.empty((frameCount, frameHeight, frameWidth, 3), np.dtype('uint8'))
+        #BCTHW
+        for i in range(count):
+            buf = np.empty((16, 244, 244,3), np.dtype('uint8'))
+            fc = 0
+            ret = True
+            while (fc < 16  and ret):
+                ret,_buf = cap.read()
+                buf[fc] = cv2.resize(_buf,(244,244))
+                fc += 1
+            buf = np.expand_dims(np.transpose(np.array(buf),(3,0,1,2)),axis=0)
+            video_array.append(buf)
 
-fc = 0
-ret = True
+        cap.release()
 
-while (fc < frameCount  and ret):
-    ret, buf[fc] = cap.read()
-    fc += 1
+    return video_array
 
-cap.release()
 
-print(frameCount)
-cv2.namedWindow('frame 10')
-cv2.imshow('frame 10', buf[9])
+# cv2.namedWindow('frame 10')
+# cv2.imshow('frame 10', buf[9])
 
-cv2.waitKey(0)
+# cv2.waitKey(0)
